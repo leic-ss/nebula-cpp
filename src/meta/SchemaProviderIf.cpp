b6ac36ff98e0210c1be82213e8147db634b77e7c
shanshenshi <shanshenshi@126.com>
2025-05-16 02:12:13 -0400

    support bulkload

---
+++ src/meta/SchemaProviderIf.cpp
@@ -0,0 +1,10 @@
+/* Copyright (c) 2018 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+
+#include "nebula/meta/SchemaProviderIf.h"
+
+namespace nebula {
+namespace meta {}  // namespace meta
+}  // namespace nebula
